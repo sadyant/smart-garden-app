b292295eb346235a7f6bad46820f5b789bf4efff
sadyant <sadyant.behera@berkeley.edu>
2021-04-04 00:22:22 -0700

    calendar

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -73,8 +73,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         height: '100%',
-        alignItems: 'center',
-        height: '100%',
     },
     topContainer: {
         paddingTop: 50,
@@ -101,64 +99,4 @@ const styles = StyleSheet.create({
     }
 })
 
-let wateringData = [
-    {
-        id: 1,
-        date: 'March 7, 2021',
-        time: '5:00 PM',
-        plant: 'Spinach'
-    },
-    {
-        id: 2,
-        date: 'March 9, 2021',
-        time: '3:00 PM',
-        plant: 'Cauliflower'
-    }
-];
-
-let waterNotifs = wateringData.map(wateringInfo => (
-    <View key={wateringInfo.id} style={styles.waterNotif}>
-        <View style={{display: 'flex', flexDirection: 'row'}}>
-            <Text style={{flex: 1, fontSize: 18, fontWeight: 'bold', paddingTop: 10, paddingLeft: 10}}>
-                {wateringInfo.date}
-            </Text>
-            <Text style={{flex: 1, fontSize: 12, fontWeight: 'bold', paddingTop: 15}}>
-                Watering{'\n'}
-            </Text>
-        </View>
-        <View style={{display: 'flex', flexDirection: 'row', paddingLeft: 10, paddingBottom: 15}}>
-            <Text style={{flex: 1, color: 'gray', fontWeight: 'bold'}}>
-                {wateringInfo.time}
-            </Text>
-            <Text style={{flex: 1, color: 'gray', fontWeight: 'bold'}}>
-                {wateringInfo.plant}
-            </Text>
-        </View>
-    </View>
-));
-
-function CalendarScreen(props) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.topContainer}>
-                <Text style={{color: '#000000', fontSize: 35, fontWeight: 'bold'}}>
-                    Welcome back!
-                </Text>
-            </View>
-            <View style={styles.middleContainer}>
-                <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-                    Upcoming Waters
-                </Text>
-                <View style={styles.waterContainer}>
-                    {waterNotifs}
-                </View>
-            </View >
-            <View style={styles.bottomContainer}>
-                <Calendar>
-
-                </Calendar>
-            </View>
-        </View>
-    );
-}
 export default CalendarScreen;
